8751fc6d986d8d4c90db3a9eb47315d669168d6e
shroida <shroida012@gmail.com>
2025-04-30 14:15:42 +0200

    call it

--- lib/payment_app.dart
+++ lib/payment_app.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/features/payment/presentation/views/widgets/my_cart_view.dart';
 
 class PaymentApp extends StatelessWidget {
   const PaymentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Center(
-      child: Text('Hello'),
-    ));
+    return const MaterialApp(home: MyCartView());
   }
 }
