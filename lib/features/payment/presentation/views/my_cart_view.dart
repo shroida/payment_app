753dee35ca782cf35be473da24fe20eb4a68a5f9
shroida <shroida012@gmail.com>
2025-04-30 14:32:38 +0200

    import

--- lib/features/payment/presentation/views/my_cart_view.dart
+++ lib/features/payment/presentation/views/my_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/core/widget/cutom_app_bar.dart';
+import 'package:payment_app/features/payment/presentation/views/widgets/my_cart_view_body.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
