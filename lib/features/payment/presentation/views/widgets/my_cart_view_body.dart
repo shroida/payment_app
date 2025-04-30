5a2956fded0e28e21ac03c426221eac387865734
shroida <shroida012@gmail.com>
2025-04-30 14:50:19 +0200

    relocate images

--- lib/features/payment/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/payment/presentation/views/widgets/my_cart_view_body.dart
@@ -10,7 +10,7 @@ class MyCartViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 200),
       child: Column(
         children: [
           const SizedBox(
