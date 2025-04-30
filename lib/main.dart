b3d16a076cba1da42deeefa68218be2790267efc
shroida <shroida012@gmail.com>
2025-04-30 14:06:33 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:payment_app/payment_app.dart';
+
+void main() {
+  runApp(const PaymentApp());
+}
---
+++ lib/payment_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PaymentApp extends StatelessWidget {
+  const PaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Center(
+      child: Text('Hello'),
+    ));
+  }
+}
