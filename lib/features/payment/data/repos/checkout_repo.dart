26077c3a41902508cb4256abfdeddfd1fcf4273d
shroida <shroida012@gmail.com>
2025-05-01 14:36:52 +0200

    CheckoutRepo done

--- lib/features/payment/data/repos/checkout_repo.dart
+++ lib/features/payment/data/repos/checkout_repo.dart
@@ -1,5 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:payment_app/core/error/failure.dart';
+import 'package:payment_app/features/payment/data/models/payment_intent_model/payment_intent_input_model.dart';
 
 abstract class CheckoutRepo {
-  Future<Either<Failure,void>> makePayment(){}
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel});
 }
