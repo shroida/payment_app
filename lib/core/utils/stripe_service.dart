c41ba3c7f0b33777064ffedadfe25ec6a75821c8
shroida <shroida012@gmail.com>
2025-05-01 08:49:21 +0200

    api service

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,1 +1,18 @@
-class StripeService {}
+import 'package:dio/dio.dart';
+
+class StripeService {
+  final Dio dio = Dio();
+  Future<Response> post(
+      {required body,
+      required String url,
+      required String token,
+      String? contentType}) async {
+    var response = await dio.post(url,
+        data: body,
+        options: Options(
+            contentType: contentType,
+            headers: {'Authorization': "Bearer $token"}));
+
+    return response;
+  }
+}
