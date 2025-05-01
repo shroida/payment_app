f4fdf18083559a118b3bf829aaad7ece62813250
shroida <shroida012@gmail.com>
2025-05-01 14:43:16 +0200

    PaymentState

--- lib/features/payment/presentation/payment/payment_state.dart
+++ lib/features/payment/presentation/payment/payment_state.dart
@@ -1,3 +1,9 @@
 sealed class PaymentState {}
 
 final class PaymentInitial extends PaymentState {}
+
+final class PaymentLoading extends PaymentState {}
+
+final class PaymentSuccess extends PaymentState {}
+
+final class PaymentFailure extends PaymentState {}
