1dcf0cb2c8efc30f0e03d56b8f98ed83fdcf6b06
shroida <shroida012@gmail.com>
2025-05-01 11:21:05 +0200

    StripeService update

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,18 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:payment_app/core/utils/api_service.dart';
+import 'package:payment_app/core/utils/constants.dart';
+import 'package:payment_app/features/payment/data/models/payment_intent_model/payment_intent_input_model.dart';
+import 'package:payment_app/features/payment/data/models/payment_intent_model/payment_intent_model.dart';
 
 class StripeService {
   final Dio dio = Dio();
-  Future<Response> createPaymentInten(
-      {required body,
-      required String url,
-      required String token,
-      String? contentType}) async {
-    var response = await dio.post(url,
-        data: body,
-        options: Options(
-            contentType: contentType,
-            headers: {'Authorization': "Bearer $token"}));
-
-    return response;
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentInten(
+      PaymentIntentInputModel paymentModel) async {
+    var response = await apiService.post(
+      body: paymentModel.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: Constants.secretKey,
+    );
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
   }
 }
